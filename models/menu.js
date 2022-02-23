f15ab127eb6ae6b0ca6be30bfe9e0da209456ff2
Shravan R Tandel <shravansrt10@gmail.com>
2022-02-23 11:40:12 +0530

    Created models folder and added menu.js which contains list of side menu

---
+++ models/menu.js
@@ -0,0 +1,12 @@
+export const sideMenu = [
+    "Recommanded",
+    "Whopper Friday (30% OFF)",
+    "New Kings Collection Burgers",
+    "Flavours of Love Combs",
+    "Home Delight Combs",
+    "Friends and Family Combo",
+    "Whopper",
+    "Meal Combos",
+    "Burgers and Wraps",
+    "What's New",
+];
