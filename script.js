1b32699884cffbe476807e4f7308998e14aa51cd
Shravan R Tandel <shravansrt10@gmail.com>
2022-02-26 17:00:37 +0530

    Added a data stub for cart and rendered cart items

--- script.js
+++ script.js
@@ -1,20 +1,29 @@
 import { sideMenu } from "./models/menu.mjs"
 import { menuItemList } from "./models/itemList.mjs"
 
-(function (){
+(function () {
+
+    let cart = [
+        {
+            name: "Whopper Fridays- Veg Doubles",
+            isVeg: true,
+            price: 298,
+            count: 2,
+        },
+    ]
 
     // function to render the side menu
     function renderSideBarMenu(sideMenuListElement) {
-        sideMenu.forEach( (menuItem, index) => {
-            sideMenuListElement.innerHTML += 
+        sideMenu.forEach((menuItem, index) => {
+            sideMenuListElement.innerHTML +=
                 `<li><a href="#${index + 1}">${menuItem}</a></li>`
         })
     }
 
-    function renderItems(itemRenderSpace) {
-        sideMenu.forEach( (menuItem, index) => {
+    function renderItems(itemRenderDiv) {
+        sideMenu.forEach((menuItem, index) => {
             // This variable stores the items in particular category
-            const itemsInCategory = menuItemList.filter( (item) => {
+            const itemsInCategory = menuItemList.filter((item) => {
                 return item.category == menuItem;
             })
 
@@ -22,12 +31,10 @@ import { menuItemList } from "./models/itemList.mjs"
             let childComponent = "";
             let numberOfitems = itemsInCategory.length;
 
-            itemsInCategory.forEach( (i) => {
-                childComponent +=  `<div class="singleItem">
+            itemsInCategory.forEach((i) => {
+                childComponent += `<div class="singleItem">
                 <div class="left">
-                    <div class="vegORnonveg">
-                        Veg
-                    </div>
+                    ${i.isVeg ? "<div class = 'veg'>Veg</div>" : "<div class = 'nonVeg'>Non Veg</div>"}
                     <div class="foodName">
                         ${i.name}
                     </div>
@@ -48,16 +55,71 @@ import { menuItemList } from "./models/itemList.mjs"
             <div class="line"></div>`
             })
 
-            itemRenderSpace.innerHTML += `<div id="${index + 1}" class="item">
+            itemRenderDiv.innerHTML += `<div id="${index + 1}" class="item">
             <div class="name">
                 <span class="n">${menuItem}</span><br>
                 <span class="i">${numberOfitems} items</span>
             </div>
             <div class="listItems">
-                ${ childComponent
-            }
+                ${childComponent
+                }
             </div>`
         })
+    }
+
+    function renderCartItems(cartRenderDiv) {
+        let cartItems = cart.length;
+        let subTotal = 0;
+        cart.forEach((i) => {
+            subTotal += (i.count * i.price);
+        })
+
+        let cartItem = "";
+
+        cart.forEach( (i) => {
+            cartItem += `<div class="cart_item">
+            <div class="cart_vegORnonveg">
+                ${ i.isVeg ? "<div class = 'veg'>Veg</div>" : "<div class = 'nonVeg'>Non Veg</div>"}
+            </div>
+            <div class="dishName">
+                ${i.name}
+            </div>
+            <div class="addSub">
+                <div class="minus">
+                    -
+                </div>
+                <div class="count">
+                    ${i.count}
+                </div>
+                <div class="plus">
+                    +
+                </div>
+            </div>
+            <div class="price">
+                ₹${i.price}
+            </div>
+        </div>`
+        })
+
+        cartRenderDiv.innerHTML = `<div class="cart_name">
+        <span class="cart1">Cart</span><br>
+        <span class="cart2">${cartItems} Item</span>
+    </div>
+    <div class="cart_items">
+        ${cartItem}
+    </div>
+    <div class="subTotal">
+        <div class="sub">
+            <span class="s1">SubTotal</span><br>
+            <span class="s2">Extra charges may apply</span>
+        </div>
+        <div class="price">
+            ₹${subTotal}
+        </div>
+    </div>
+    <div class="checkout">
+        <button class="button">Checkout</button>
+    </div>`
     }
 
     function render() {
@@ -68,12 +130,17 @@ import { menuItemList } from "./models/itemList.mjs"
         renderSideBarMenu(sideMenuListELement);
 
         // selecting div where we are rednering all items according to their category
-        const itemRenderSpace = document.querySelector(".items")
+        const itemRenderDiv = document.querySelector(".items")
 
         // function for rendering items
-        renderItems(itemRenderSpace);
+        renderItems(itemRenderDiv);
+
+        // selecting div where we are rendering all the cart items
+        const cartRenderDiv = document.querySelector(".menuAndCart .cart");
+
+        renderCartItems(cartRenderDiv);
     }
 
-    
+
     render();
 })();
